943aefc083aade5f200d74a845646d45ce0816e2
Leaf Eriksen <leaf@Leafs-MacBook-Pro.local>
2024-12-13 23:38:26 -0800

    fixed hotheys

--- .config/setup/brew.sh
+++ .config/setup/brew.sh
@@ -2,15 +2,13 @@
 
 softwareupdate --install-rosetta --agree-to-license
 
-brew install --formula lazygit less mas eza nano zsh-autosuggestions zsh-syntax-highlighting fswatch yt-dlp ffmpeg 
+brew install --formula lazygit less mas eza nano zsh-autosuggestions zsh-syntax-highlighting fswatch yt-dlp ffmpeg koekeishiya/formulae/skhd
+
+skhd --start-service
 
 brew install --cask --no-quarantine font-atkinson-hyperlegible font-open-dyslexic font-red-hat-mono sanesidebuttons aldente maccy notunes iina proton-pass obsidian librewolf zotero affinity-designer calibre soulver anki itch steam whisky heroic modrinth utm webstorm knockknock chromium loop
 
 ###############################################################################
 
-# deno tailwindcss
-# brew install koekeishiya/formulae/skhd
-# skhd --start-service
-# brew tap zackelia/formulae && brew install bclm
-# brew tap FelixKratz/formulae && brew install borders && brew services start borders
+# deno tailwindcss zackelia/formulae/bclm FelixKratz/formulae/borders && brew services start borders
 # thunderbird@esr libreoffice handbrake vlc zen-browser porting-kit crossover diffusionbee lunar topnotch
