7b7139997e77df23e8579adf9971f9a90f67ff06
Leaf Eriksen <leaf@Leafs-MacBook-Pro.local>
2024-12-13 21:20:53 -0800

    separate login file

---
+++ .config/setup/login.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/Maccy.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/Lunar.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/AlDente.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/LuLu.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Pure Paste.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/noTunes.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/SaneSideButtons.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/Hyperkey.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Users/leaf/Applications/Loop.app", hidden:false}'
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Lickable Menu Bar.app", hidden:false}'
+osascript -e 'tell application "System Events" to get the name of every login item'
