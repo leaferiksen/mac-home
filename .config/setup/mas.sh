611020d442c367514e2432a7e40deb3953408550
Leaf Eriksen <leaf@Leafs-MacBook-Pro.local>
2024-12-13 19:20:13 -0800

    stuff after reset

---
+++ .config/setup/mas.sh
@@ -0,0 +1,21 @@
+mas install 6449873635  #Sink It for Reddit
+mas install 1592917505  #Noir
+mas install 425424353   #The Unarchiver
+mas install 497799835   #Xcode
+mas install 1591303229  #Vinegar
+mas install 1611378436  #Pure Paste
+mas install 6444217677  #Lickable Menu Bar
+mas install 899247664   #TestFlight
+mas install 6720708363  #Obsidian Web Clipper
+mas install 1601151613  #Baking Soda
+mas install 6502835663  #Proton Pass for Safari
+mas install 1463298887  #Userscripts-Mac-App
+mas install 462054704   #Microsoft Word
+mas install 463981119   #NewsBlur
+mas install 1547912640  #uBlacklist for Safari
+mas install 462062816   #Microsoft PowerPoint
+mas install 1475387142  #Tailscale
+mas install 1662217862  #Wipr
+mas install 462058435   #Microsoft Excel
+# mas install 1604385869  #ManGo
+# open https://apps.apple.com/us/app/mango-anime-manga-tracker/id1604385869
