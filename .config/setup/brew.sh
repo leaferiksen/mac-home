6ae5f9c8dfb1daccc76fa9ed619098b856f0d6a5
Leaf Eriksen <leaferiksen@gmail.com>
2025-02-11 21:15:59 -0800

    stuff

--- .config/setup/brew.sh
+++ .config/setup/brew.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-brew install --formula eza fastfetch ffmpeg fswatch jackett koekeishiya/formulae/skhd lazygit less mas nano tailwindcss tlrc yt-dlp zellij zsh-autosuggestions zsh-syntax-highlighting
+brew install --formula duti eza fastfetch ffmpeg fswatch jackett koekeishiya/formulae/skhd lazygit less mas nano tailwindcss tlrc yt-dlp zsh-autosuggestions zsh-syntax-highlighting
 
-brew install --cask --no-quarantine aldente anki calibre chromium font-atkinson-hyperlegible font-open-dyslexic font-red-hat-mono ghostty iina itch knockknock loop maccy notunes obsidian osu proton-pass qbittorrent sanesidebuttons soulver steam utm webstorm zen-browser zotero
+brew install --cask --no-quarantine aldente anki calibre chromium font-atkinson-hyperlegible font-open-dyslexic font-red-hat-mono ghostty iina itch knockknock loop maccy notunes obsidian osu proton-pass qbittorrent@lt20 sanesidebuttons soulver steam utm webstorm zen-browser zotero
 
 brew services start jackett
 
 ###############################################################################
 
-# brew uninstall --formula 
+# brew uninstall --formula
 # brew uninstall --cask --zap
 # thunderbird@esr libreoffice handbrake vlc diffusionbee lunar topnotch whisky heroic
