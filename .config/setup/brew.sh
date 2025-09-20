13be20d9561218dab1d03606af0f0c7d2143708d
Leaf Eriksen <leaferiksen@gmail.com>
2025-09-19 22:54:09 -0700

    pgp working

--- .config/setup/brew.sh
+++ .config/setup/brew.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-brew install --formula aria2 d12frosted/emacs-plus/emacs-plus@30 duti enchant eza ffmpeg mas node tealdeer yt-dlp zsh-autosuggestions zsh-syntax-highlighting xcodesorg/made/xcodes
+brew install --formula aria2 d12frosted/emacs-plus/emacs-plus@30 duti enchant eza ffmpeg gnupg mas node pinentry-mac tealdeer yt-dlp zsh-autosuggestions zsh-syntax-highlighting xcodesorg/made/xcodes
 
 brew tap homielab/mountmate https://github.com/homielab/mountmate
-brew install --cask anki anybar calibre chromium font-atkinson-hyperlegible-next font-maple-mono ghostty hammerspoon iina itch kanriapp/cask/kanri knockknock loop mountmate node notunes obsidian osu qlmarkdown qlvideo soulver steam syncthing zen-browser zotero
+brew install --cask anki anybar calibre chromium cursorcerer font-atkinson-hyperlegible-next font-maple-mono-nf-cn ghostty hammerspoon iina itch kanriapp/cask/kanri knockknock loop mountmate node notunes obsidian osu qlmarkdown qlvideo soulver steam syncthing zen-browser zotero
 
 npm install -g npm-check-updates eslint http-server @tailwindcss/language-server prettier rustywind typescript typescript-language-server vscode-langservers-extracted
 
--- .zshenv
+++ .zshenv
@@ -1,3 +1,3 @@
 # brew
-export PATH=/Users/leaf/bin:/opt/homebrew/sbin:/opt/homebrew/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/Apple/usr/bin
+export PATH=/Users/leaf/bin:/opt/homebrew/sbin:/opt/homebrew/bin:$PATH
 export HOMEBREW_CASK_OPTS="--appdir=~/Applications --no-quarantine"
